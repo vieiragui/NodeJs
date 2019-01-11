e648b03838b64db53daa6ef30add9866c0f69dd6
vieiragui <vieiragui21@gmail.com>
2019-01-11 18:45:58 -0200

    Criacao gitignore e atualizao do index principal

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ let app = express();//retorna toda a aplicaçao, conjunto de informações que e
 //get é o metodo que esta sendo utilizado. estou criando uma rota que é acessivel pelo metodo get
 //primeiro parametro é a rota
 app.get('/',(req, res) => { //criando servidor. res são todas as chamadas(solicitações) e res são todas as respostas
-    //console.log('URL: ', req.url);//exibe a url que esta sendo chamada
-    //console.log('METHOD: ', req.method);//exibe o método que esta sendo chamado
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Ola</h1>');
